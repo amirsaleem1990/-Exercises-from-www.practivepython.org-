791cc26e8d4a5550b3672978afa52633993e1315
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2018-01-13 16:03:49 +0500

    updated

--- Exercise #5.py
+++ Exercise #5.py
@@ -12,6 +12,18 @@ Extras:
 2- Write this in one line of Python (don’t worry if you can’t figure this out at this
 point - we’ll get to it soon)
 '''
+# v1
+from random import randint as r
+a = [i for i in range(r(0,10))]
+b = [i for i in range(r(0,10))]
+def list_overlap(list1, list2):
+    print('list1: \n', [i for i in list1])# deleting this line from code not affect our code
+    print('list2: \n', [i for i in list2])# deleting this line from code not affect our code
+    print('common elements in two lists: ')# deleting this line from code not affect our code
+    return list(set([i for i in b if i in a]))
+print(list_overlap(a, b))
+'''
+# v2
 import random
 a = [int(random.choice('0123456789')) for i in range(8)]
 b = [int(random.choice('0123456789')) for i in range(6)]
@@ -21,3 +33,4 @@ def list_overlap(list1, list2):
     print('common elements in two lists: ')
     return list(set([i for i in b if i in a]))
 print(list_overlap(a, b))
+'''
